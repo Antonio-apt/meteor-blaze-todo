45d582d2d981ff02eabbfe0d8871f7cfbe0f8950
Antonio Souza <antonio.neto@madeinweb.com.br>
2021-07-14 13:44:17 -0300

    remove build folder

--- .meteor/local/bundler-cache/scanner/web.browser.legacy/reify-60600e0975115dca5c9d6781b4988eef3104ce8b.js
+++
@@ -1,3 +0,0 @@
-module.export({TasksCollection:function(){return TasksCollection}},true);var Mongo;module.link("meteor/mongo",{Mongo:function(v){Mongo=v}},0);
-
-const TasksCollection = new Mongo.Collection("tasks");
--- .meteor/local/bundler-cache/scanner/web.browser/reify-60600e0975115dca5c9d6781b4988eef3104ce8b.js
+++
@@ -1,3 +0,0 @@
-module.export({TasksCollection:()=>TasksCollection},true);let Mongo;module.link("meteor/mongo",{Mongo(v){Mongo=v}},0);
-
-const TasksCollection = new Mongo.Collection("tasks");
